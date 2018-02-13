5d465f9823e0f421955495c5e3a9af6203c2344c
guiguetmarcelo <33025113+guiguetmarcelo@users.noreply.github.com>
2018-02-13 00:45:24 -0300

    diseño home

--- TPFinal_POO2017/src/main/java/ar/edu/unnoba/poo2017/tpfinal_poo2017/model/AbstractEntity.java
+++ TPFinal_POO2017/src/main/java/ar/edu/unnoba/poo2017/tpfinal_poo2017/model/AbstractEntity.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ar.edu.unnoba.poo2017.tpfinal_poo2017.model;
 
 import java.util.Objects;
@@ -17,7 +13,7 @@ import javax.persistence.MappedSuperclass;
 
 /**
  *
- * @author poo
+ * @author Marcelo
  */
 
 @MappedSuperclass
