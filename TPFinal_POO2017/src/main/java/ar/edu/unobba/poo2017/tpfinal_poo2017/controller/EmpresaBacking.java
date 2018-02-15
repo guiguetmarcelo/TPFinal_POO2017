f2790c6400fcf70b8aba4e655b139fa05a593b80
sebastian997et <sadorno@comunidad.unnoba.edu.ar>
2018-02-14 22:24:07 -0300

    i18n empresa

--- TPFinal_POO2017/src/main/java/ar/edu/unobba/poo2017/tpfinal_poo2017/controller/EmpresaBacking.java
+++ TPFinal_POO2017/src/main/java/ar/edu/unobba/poo2017/tpfinal_poo2017/controller/EmpresaBacking.java
@@ -63,7 +63,7 @@ public class EmpresaBacking implements Serializable {
         } catch (Exception e) {
             FacesContext context = FacesContext.getCurrentInstance();
             FacesMessage message;
-            message = new FacesMessage("ERROR al registrar la empresa. Posible causa: La empresa ya existe");
+            message = new FacesMessage(msg.getString("empresa_existente"));
             context.addMessage("msgEmpresa", message);
             return null;
         }
@@ -97,7 +97,7 @@ public class EmpresaBacking implements Serializable {
         } catch (Exception e) {
             FacesContext context = FacesContext.getCurrentInstance();
             FacesMessage message;
-            message = new FacesMessage("ERROR al eliminar la empresa. Posible causa: La empresa esta en uso");
+            message = new FacesMessage(msg.getString("empresa_enuso"));
             context.addMessage("msgEmpresa", message);
             
         }
